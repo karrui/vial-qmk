3aa9bec4ef221a1cf984bd76e53e3dd73e6adcf2
Kar Rui Lau <karrui.lau@gmail.com>
2022-02-13 16:01:29 +0800

    feat: update tmkl keymap for saner defaults

--- keyboards/mechlovin/tmkl/keymaps/vial/config.h
+++ keyboards/mechlovin/tmkl/keymaps/vial/config.h
@@ -3,5 +3,5 @@
 #pragma once
 
 #define VIAL_KEYBOARD_UID { 0x21, 0x78, 0x08, 0x82, 0x26, 0x1C, 0xFE, 0x6D }
-#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
